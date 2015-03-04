957a914fb34c11ce338d2203bd4a953b98a71649
Jesenko <jeshko2@hotmail.com>
2015-03-04 12:22:49 +0100

    remove error import

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,6 +3,30 @@
 
 # --- !Ups
 
+create table coupon (
+  id                        bigint not null,
+  name                      varchar(255),
+  description               varchar(255),
+  picture                   varchar(255),
+  category_id               bigint,
+  code                      varchar(255),
+  last_minute               boolean,
+  created                   timestamp,
+  duration                  double,
+  ending                    timestamp,
+  special_price             boolean,
+  price                     double,
+  view_count                integer,
+  special_offer             boolean,
+  multi_offer_id            bigint,
+  status                    boolean,
+  company_id                bigint,
+  remark                    varchar(255),
+  comment_user_id           bigint,
+  response_company_id       bigint,
+  constraint pk_coupon primary key (id))
+;
+
 create table user (
   id                        bigint not null,
   username                  varchar(255),
@@ -10,6 +34,8 @@ create table user (
   password                  varchar(255),
   constraint pk_user primary key (id))
 ;
+
+create sequence coupon_seq;
 
 create sequence user_seq;
 
@@ -20,9 +46,13 @@ create sequence user_seq;
 
 SET REFERENTIAL_INTEGRITY FALSE;
 
+drop table if exists coupon;
+
 drop table if exists user;
 
 SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists coupon_seq;
 
 drop sequence if exists user_seq;
 
