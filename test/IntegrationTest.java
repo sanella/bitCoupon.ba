16bcd1331f7dc0bc884c932adace4a8cd21f1300
Jesenko <jeshko2@hotmail.com>
2015-03-04 15:06:31 +0100

    continue tests from sprint 1

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,3 +1,5 @@
+import models.User;
+
 import org.junit.*;
 
 import play.mvc.*;
@@ -6,6 +8,7 @@ import play.libs.F.*;
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
 import static org.fluentlenium.core.filter.FilterConstructor.*;
+import static org.junit.Assert.*;
 
 public class IntegrationTest {
 
@@ -19,47 +22,57 @@ public class IntegrationTest {
             public void invoke(TestBrowser browser) {
                 browser.goTo("http://localhost:3333");
                 assertThat(browser.pageSource()).contains("Welcome ");
-                assertThat(browser.pageSource()).contains("Welcome to bitCoupon");
-                assertThat(browser.pageSource()).contains("Home: ");
                 assertThat(browser.pageSource()).contains("Registration");
                 assertThat(browser.pageSource()).contains("Login");
-                 assertThat(browser.pageSource()).contains("Logout");
+          
             }
         });
     }
-  /*
+
+ 
    @Test
     public void testRegistration() {
         running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
   
-                browser.goTo("http://localhost:3333/");
+                browser.goTo("http://localhost:3333/registration");
                 browser.fill("#username").with("tester");
-                browser.fill("#email").with("test.testoviic@bitcamp.ba");
+                browser.fill("#email").with("tester@mail.com");
                 browser.fill("#password").with("123456");
-                browser.submit("#register");
-                assertThat(browser.pageSource()).contains("Welcome tester");
+                browser.fill("#confirmPassword").with("123456");
+                browser.submit("#submit");
+                assertThat(browser.pageSource()).contains("Welcome  tester");
                 assertThat(browser.pageSource()).contains("Home: ");
                 assertThat(browser.pageSource()).contains("Logout");
 
             }
         });
-    }*/
+    }
+   
+   
    
    @Test
    public void testLogin() {
        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
            public void invoke(TestBrowser browser) {
- 
-               browser.goTo("http://localhost:3333/");
+        	 //  User.createUser("tester", "tester@mail.com", "123456");
+        	   
+        	   browser.goTo("http://localhost:3333/registration");
                browser.fill("#username").with("tester");
+               browser.fill("#email").with("tester@mail.com");
                browser.fill("#password").with("123456");
-               browser.submit("#login");
-               assertThat(browser.pageSource()).contains("Welcome tester");
+               browser.fill("#confirmPassword").with("123456");
+               browser.submit("#submit");
+        	   
+        	   browser.goTo("http://localhost:3333/loginpage");
+               browser.fill("#email").with("tester@mail.com");
+               browser.fill("#password").with("123456");
+               browser.submit("#submit");
+               assertThat(browser.pageSource()).contains("Welcome  tester@mail.com");
                assertThat(browser.pageSource()).contains("Home: ");
                assertThat(browser.pageSource()).contains("Logout");
 
            }
        });
-   }
+   } 
 }
