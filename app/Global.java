fa98f3652375fc5bde1dbf97869409f96e0b4c5b
Uqluiorrae <hariskrkalic@gmail.com>
2015-03-05 18:13:10 +0100

    add admin rule and global admin in DB

--- app/Global.java
+++ app/Global.java
@@ -1,3 +1,4 @@
+import helpers.HashHelper;
 import models.Coupon;
 import models.User;
 import play.Application;
@@ -16,5 +17,8 @@ public class Global extends GlobalSettings {
 		
 		Coupon.createCoupon("Vikend u Neumu", 80, "04.03.2015", "11.03.2015", "http://i.imgur.com/n94Jv7I.gif", 
 				"Putovanja", "Vikend za dvoje u Neumu", "Vikend za dvoje u Neumu");
+		
+		User.createUser("Admin", "admin@mail.com", HashHelper.createPassword("bitCoupon"), true);
+		
 	}
 }
