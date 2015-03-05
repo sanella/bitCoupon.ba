875e0c217157d84943ae000dc1e6f6a3eea022cd
Uqluiorrae <hariskrkalic@gmail.com>
2015-03-05 17:12:39 +0100

    added some hotfixes

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -59,10 +59,11 @@ public class UserController extends Controller {
 		 there are no errors */
 		
 		 else if ( User.verifyRegistration(username, mail) == true){
+		 session().clear();
 		 session("name", mail);
 		 long id = User.createUser(username, mail, hashPass, false);
 		 return redirect("/user/" + id);
-		
+		 
 		 } else {
 		 return ok(signup.render("Username or email allready exists!",
 		 username, mail ));
@@ -75,7 +76,7 @@ public class UserController extends Controller {
 	public static Result show(long id){
 		
 		User u = User.find(id);
-		if ( !u.username.equals(session("name"))){
+		if ( !u.email.equals(session("name"))){
 			return redirect("/");
 		}
 		
