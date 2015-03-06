995e48566b6768dd2ddc79ff963d2af3626dbab5
Jesenko <jeshko2@hotmail.com>
2015-03-06 07:44:19 +0100

    chenge admin pass to bitadmin

--- app/Global.java
+++ app/Global.java
@@ -12,13 +12,13 @@ public class Global extends GlobalSettings {
 		Coupon.createCoupon("Vikend u Neumu", 80, "04.03.2015", "11.03.2015", "http://i.imgur.com/n94Jv7I.gif", 
 				"Putovanja", "Vikend za dvoje u Neumu", "Vikend za dvoje u Neumu");
 		
-		Coupon.createCoupon("Vikend u Neumu", 80, "04.03.2015", "11.03.2015", "http://i.imgur.com/n94Jv7I.gif", 
-				"Putovanja", "Vikend za dvoje u Neumu", "Vikend za dvoje u Neumu");
+		Coupon.createCoupon("Teretana 2u1", 40, "06.03.2015", "06.05.2015", "http://i.imgur.com/n94Jv7I.gif", 
+				"Sport", "Jedan mjesec gratis treniranja", "Najnovije sprave");
 		
-		Coupon.createCoupon("Vikend u Neumu", 80, "04.03.2015", "11.03.2015", "http://i.imgur.com/n94Jv7I.gif", 
-				"Putovanja", "Vikend za dvoje u Neumu", "Vikend za dvoje u Neumu");
+		Coupon.createCoupon("Vecera za dvoje", 70, "07.03.2015", "16.03.2015", "http://i.imgur.com/n94Jv7I.gif", 
+				"Food", "Vecera za dvije osobe 30% jeftinije", "Ekskluzivna ponuda");
 		
-		User.createUser("Admin", "admin@mail.com", HashHelper.createPassword("bitCoupon"), true);
+		User.createUser("Admin", "admin@mail.com", HashHelper.createPassword("bitadmin"), true);
 		
 	}
 }
