33aba8ed5903cc791f343bb74f05b7def0f36b7f
Emir Imamovic <emir92@outlook.com>
2015-03-20 06:02:52 +0100

    add category container

--- app/Global.java
+++ app/Global.java
@@ -11,7 +11,6 @@ public class Global extends GlobalSettings {
 	String nameCoupon1 = "Dvije noći za dvoje u Hotelu Sunce Neum";
 	String remarkCoupon1 = "Jedna osoba može kupiti maksimalno četiri kupona za ovu ponudu. Kuponi se mogu spajati.";
 
-
 	String descriptionCoupon1 = "Poželjeli ste da udahnete miris mora i da na bar dva dana pobjegnete od svakodnevnice, da se opustite uz duge šetnje plažom? Uživajte u dvije noći u Hotelu \"Sunce\" u Neumu za dvije osobe uz doručak!";
 
 	String nameCoupon2 = "Mesec dana korištenja teretane + kardio program ! Posljednje pripreme pred ljeto!";
@@ -27,26 +26,30 @@ public class Global extends GlobalSettings {
 			+ "Hotel Brass unikatan po mnogo čemu: dizajnu, usluzi te uslužnom osoblju."
 			+ "Upravo ovakav ambijent začinit će i uljepšati Vašu romantičnu večeru.";
 
-
-	
-
-
 	@Override
 	public void onStart(Application app) {
+		Category food = null;
+		Category travel = null;
+		Category sport = null;
 		
-		Category food = new Category("Food");
-		food.save();
-		Category travel = new Category("Travel");
-		travel.save();
-		Category sport = new Category("Sport");
-		sport.save();
-		
+		if (!Category.checkByName("Food")) {
+			food = new Category("Food");
+			food.save();
+		}
+		if (!Category.checkByName("Travel")) {
+			travel = new Category("Travel");
+			travel.save();
+		}
+
+		if (!Category.checkByName("Sport")) {
+			sport = new Category("Sport");
+			sport.save();
+		}
 
 		if (Coupon.checkByName(nameCoupon1) == false) {
 			Coupon.createCoupon(nameCoupon1, 80, null,
 					"http://static.panoramio.com/photos/large/26139268.jpg",
-			travel,descriptionCoupon1,
-					remarkCoupon1);
+					travel, descriptionCoupon1, remarkCoupon1);
 		}
 		if (Coupon.checkByName(nameCoupon2) == false) {
 			Coupon.createCoupon(
@@ -54,8 +57,7 @@ public class Global extends GlobalSettings {
 					40,
 					null,
 					"http://www.thepullforhumanity.com/anytime_fitness/wp-content/gallery/general/gym-pics-097.jpg",
-					sport, descriptionCoupon2,
-					remarkCoupon2);
+					sport, descriptionCoupon2, remarkCoupon2);
 		}
 		if (Coupon.checkByName(nameCoupon3) == false) {
 			Coupon.createCoupon(
@@ -63,8 +65,7 @@ public class Global extends GlobalSettings {
 					20,
 					null,
 					"http://www.mitara.com/wp-content/uploads/2015/02/Candle-Light-Dinner-With-Romantic-Design-For-Beautiful-Valentine-Candle-Light-Dinner-Inspiring-Design-Ideas.jpg",
-					food, descriptionCoupon3,
-					remarkCoupon3);
+					food, descriptionCoupon3, remarkCoupon3);
 		}
 
 		if (User.check("admin@mail.com") == false) {
@@ -73,48 +74,53 @@ public class Global extends GlobalSettings {
 			EmailVerification setVerified = new EmailVerification(1, true);
 			setVerified.save();
 		}
-		
+
 		if (User.check("john@mail.com") == false) {
 			User.createUser("John", "john@mail.com",
 					HashHelper.createPassword("johndoe"), false);
 			EmailVerification setVerified = new EmailVerification(2, true);
 			setVerified.save();
 		}
-		
-		
-		if (FAQ.checkByQuestion("Želim kupiti današnju ponudu. Kako da to učinim?") == false){
-			FAQ.createFAQ("Želim kupiti današnju ponudu. Kako da to učinim?", "Morate biti "
-					+ "registrovani i ulogovani na stranicu. Ukoliko još nemate nalog,"
-					+ " registracija je besplatna i traje manje od 30 sekundi, a također se "
-					+ "možete uvezati sa Vašim Facebook nalogom. Kada se ulogujete kliknite na"
-					+ " dugme Kupi i pratite jednostavne korake.");
+
+		if (FAQ.checkByQuestion("Želim kupiti današnju ponudu. Kako da to učinim?") == false) {
+			FAQ.createFAQ(
+					"Želim kupiti današnju ponudu. Kako da to učinim?",
+					"Morate biti "
+							+ "registrovani i ulogovani na stranicu. Ukoliko još nemate nalog,"
+							+ " registracija je besplatna i traje manje od 30 sekundi, a također se "
+							+ "možete uvezati sa Vašim Facebook nalogom. Kada se ulogujete kliknite na"
+							+ " dugme Kupi i pratite jednostavne korake.");
 		}
-		
-		if (FAQ.checkByQuestion("Na koji način plaćam ponudu i je li sigurno?") == false){
-			FAQ.createFAQ("Na koji način plaćam ponudu i je li sigurno?", "Plaćanje možete obaviti na"
-					+ " jedan od sljedećih načina:"
-					+"1.	Putem eKredita – eKredit možete uplatiti u banci ili pošti na naš transkcijski "
-					+ "račun, a on će Vam biti dodijeljen sljedeći radni dan ili najkasnije 48 sati nakon"
-					+ " uplate. Kada Vaš eKredit bude prikazan na Vašem profilu možete obaviti željenu kupovinu. "
-					+ "Više informacija možete pronaći na ovom linku nakon prijave na Vaš profil."
-					+"2.	Putem kartice – Plaćanje je zbog sigurnosti, jednostavnije realizacije i brzine "
-					+ "obrade zahtjeva limitirano na kreditne i debitne MasterCard, Maestro i VISA kartice. "
-					+ "Plaćanje je potpuno sigurno, a više o tome možete pročitati na - sigurnost plaćanja."
-					+"3.	Dio plaćate eKreditom, a dio karticom – Prilikom kupovine potrebno je da unesete iznos "
-					+ "eKredita koji želite da Vam se odbije i dalje idete na „Nastavi kupovinu“. Unesete sve"
-					+ " potrebne podatke i na taj način će Vam se jedan dio odbiti u vidu eKredita a ostatak"
-					+ " vrijednosti će biti rezervisan na Vašoj kartici."
-					+"4. Gotovinska uplata kupona ili ekredita - uplatu možete izvršiti na eKupon prodajnim mjestima u"
-					+ " BBI tržnom centru i Grand centru na Ilidži. "
-					+"5. Kupovina poklon bona na ime i prezime osobe koju želite bez obzira da li je registrovan korisnik ili ne.");
+
+		if (FAQ.checkByQuestion("Na koji način plaćam ponudu i je li sigurno?") == false) {
+			FAQ.createFAQ(
+					"Na koji način plaćam ponudu i je li sigurno?",
+					"Plaćanje možete obaviti na"
+							+ " jedan od sljedećih načina:"
+							+ "1.	Putem eKredita – eKredit možete uplatiti u banci ili pošti na naš transkcijski "
+							+ "račun, a on će Vam biti dodijeljen sljedeći radni dan ili najkasnije 48 sati nakon"
+							+ " uplate. Kada Vaš eKredit bude prikazan na Vašem profilu možete obaviti željenu kupovinu. "
+							+ "Više informacija možete pronaći na ovom linku nakon prijave na Vaš profil."
+							+ "2.	Putem kartice – Plaćanje je zbog sigurnosti, jednostavnije realizacije i brzine "
+							+ "obrade zahtjeva limitirano na kreditne i debitne MasterCard, Maestro i VISA kartice. "
+							+ "Plaćanje je potpuno sigurno, a više o tome možete pročitati na - sigurnost plaćanja."
+							+ "3.	Dio plaćate eKreditom, a dio karticom – Prilikom kupovine potrebno je da unesete iznos "
+							+ "eKredita koji želite da Vam se odbije i dalje idete na „Nastavi kupovinu“. Unesete sve"
+							+ " potrebne podatke i na taj način će Vam se jedan dio odbiti u vidu eKredita a ostatak"
+							+ " vrijednosti će biti rezervisan na Vašoj kartici."
+							+ "4. Gotovinska uplata kupona ili ekredita - uplatu možete izvršiti na eKupon prodajnim mjestima u"
+							+ " BBI tržnom centru i Grand centru na Ilidži. "
+							+ "5. Kupovina poklon bona na ime i prezime osobe koju želite bez obzira da li je registrovan korisnik ili ne.");
 		}
-		
-		if (FAQ.checkByQuestion("Šta se dešava ako ponuda dana ne dosegne minimalan broj kupaca?")==false){
-			FAQ.createFAQ("Šta se dešava ako ponuda dana ne dosegne minimalan broj kupaca?", "Da bi ponuda "
-					+ "uspjela mora dostići minimalan broj kupaca koji se određuje u dogovoru s partnerima."
-					+ "U slučaju da se ne kupi dovoljan broj kupona, ponuda propada i ništa Vam se ne naplaćuje. "
-					+ "Da izbjegnete ovakve situacije, pozovite što više osoba da se uključe na ponudu na "
-					+ "kojoj ste učestvovali.");
+
+		if (FAQ.checkByQuestion("Šta se dešava ako ponuda dana ne dosegne minimalan broj kupaca?") == false) {
+			FAQ.createFAQ(
+					"Šta se dešava ako ponuda dana ne dosegne minimalan broj kupaca?",
+					"Da bi ponuda "
+							+ "uspjela mora dostići minimalan broj kupaca koji se određuje u dogovoru s partnerima."
+							+ "U slučaju da se ne kupi dovoljan broj kupona, ponuda propada i ništa Vam se ne naplaćuje. "
+							+ "Da izbjegnete ovakve situacije, pozovite što više osoba da se uključe na ponudu na "
+							+ "kojoj ste učestvovali.");
 		}
 
 	}
--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,45 @@
 /*price range*/
 
- $('#sl2').slider();
+// $('#sl2').slider();
 
 	var RGBChange = function() {
 	  $('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
 	};	
 		
-/*scroll to top*/
+/* scroll to top */
 
 $(document).ready(function(){
+	$('.child').mouseover(function(){
+		var index = $('.child').index(this);
+		$('.image').eq(index).css({'background-position':'50px 0px'});
+	});
+	
+	$('.child').mouseout(function(){
+		var index = $('.child').index(this);
+		$('.image').eq(index).css({'background-position':'7px 0px'});
+	});
+	
 	$(function () {
 		$.scrollUp({
 	        scrollName: 'scrollUp', // Element ID
-	        scrollDistance: 300, // Distance from top/bottom before showing element (px)
+	        scrollDistance: 300, // Distance from top/bottom before showing
+									// element (px)
 	        scrollFrom: 'top', // 'top' or 'bottom'
 	        scrollSpeed: 300, // Speed back to top (ms)
-	        easingType: 'linear', // Scroll to top easing (see http://easings.net/)
+	        easingType: 'linear', // Scroll to top easing (see
+									// http://easings.net/)
 	        animation: 'fade', // Fade, slide, none
 	        animationSpeed: 200, // Animation in speed (ms)
-	        scrollTrigger: false, // Set a custom triggering element. Can be an HTML string or jQuery object
-					//scrollTarget: false, // Set a custom target element for scrolling to the top
-	        scrollText: '<i class="fa fa-angle-up"></i>', // Text for element, can contain HTML
+	        scrollTrigger: false, // Set a custom triggering element. Can be
+									// an HTML string or jQuery object
+					// scrollTarget: false, // Set a custom target element for
+					// scrolling to the top
+	        scrollText: '<i class="fa fa-angle-up"></i>', // Text for element,
+															// can contain HTML
 	        scrollTitle: false, // Set a custom <a> title if required.
 	        scrollImg: false, // Set true to use image
-	        activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+	        activeOverlay: false, // Set CSS color to display scrollUp active
+									// point, e.g '#00FFFF'
 	        zIndex: 2147483647 // Z-Index for the overlay
 		});
 	});
